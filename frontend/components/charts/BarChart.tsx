48105302bb386a85513704b2590dca825dda19e5
yood2 <yood2@proton.me>
2024-09-05 23:30:38 -0700

    make BarChart component

---
+++ frontend/components/charts/BarChart.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { ResponsiveContainer, BarChart as RechartsBar, Bar, XAxis, YAxis, Tooltip, CartesianGrid } from 'recharts';
+
+const BarChart: React.FC<{ data: any }> = ({ data }) => {
+  const chartData = data.labels.map((label: string, index: number) => ({
+    name: label,
+    value: data.data[index]
+  }));
+
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <RechartsBar data={chartData}>
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
+        <Bar dataKey="value" fill="#8884d8" />
+      </RechartsBar>
+    </ResponsiveContainer>
+  );
+};
+
+export default BarChart;
