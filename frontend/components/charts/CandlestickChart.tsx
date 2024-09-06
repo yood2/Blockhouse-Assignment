be18a49b7c3248ec2b573a482f2544e181689864
yood2 <yood2@proton.me>
2024-09-05 23:29:09 -0700

    make CandlestickChart component

---
+++ frontend/components/charts/CandlestickChart.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ResponsiveContainer, CandlestickChart as RechartsCandle, XAxis, YAxis, Tooltip, Candlestick } from 'recharts';
+
+const CandlestickChart: React.FC<{ data: any }> = ({ data }) => {
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <RechartsCandle data={data.data}>
+        <XAxis dataKey="x" />
+        <YAxis domain={['auto', 'auto']} />
+        <Tooltip />
+        <Candlestick />
+      </RechartsCandle>
+    </ResponsiveContainer>
+  );
+};
+
+export default CandlestickChart;
